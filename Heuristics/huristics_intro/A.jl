36dd5e586a6fb4e815d896369e26f53c98d3c1cf
M315 <marc.jovanibertran@gmail.com>
2021-10-18 14:32:06 +0200

    greedy is not that bad

--- Heuristics/huristics_intro/A.jl
+++ Heuristics/huristics_intro/A.jl
@@ -1,9 +1,39 @@
-function schedule(D, c, s, stream::IO)
+function find_max(i, c, s, days_no_contest)
+    best = 1
+    max = typemin(Int32)
     
-    for i in 1:D
-        println(stream, 1)
+    total_penalty = 0
+    for k in 1:26
+        total_penalty += (c[k] * days_no_contest[k])
     end
     
+    for k in 1:26
+        penalty = c[k] * days_no_contest[k]
+        score = s[k] - total_penalty + penalty
+        if score > max
+            max = score
+            best = k
+        end
+    end
+    
+    return best
+end
+
+
+
+function schedule(D, c, s, stream::IO)
+    days_no_contest = fill(1, 26)
+    
+    for i in 1:D
+        day = find_max(i, c, s[i], days_no_contest)
+        println(stream, day)
+        
+        for j in 1:26
+            days_no_contest[j] += 1
+        end
+        
+        days_no_contest[day] = 0
+    end
 end
 
 function main()
